bace59db74f489e9632bb8be72b9cb7eb3cfc7f9
Sean Perkins <sean@ionic.io>
2023-08-10 09:41:01 -0500

    chore(angular): generate standalone component wrappers

--- packages/angular/standalone/src/index.ts
+++ packages/angular/standalone/src/index.ts
@@ -4,3 +4,4 @@ export { IonPopover } from './overlays/popover';
 export { IonRouterOutlet } from './navigation/router-outlet';
 export { IonRouterLink, IonRouterLinkWithHref } from './navigation/router-link-delegate';
 export { IonNav } from './navigation/nav-delegate';
+export * from './directives/proxies';
